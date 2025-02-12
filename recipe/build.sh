f8940572d27b628c5fb4040ae26fc40952d67d2f
Raul P. Pelaez <raulppelaez@gmail.com>
2025-02-12 11:17:12 +0100

    Enable CUDA builds

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,8 @@ cmake ${CMAKE_ARGS} -S ${SRC_DIR} -B build \
 -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_INSTALL_PREFIX=$PREFIX \
 -DCMAKE_INSTALL_LIBDIR=lib \
--DBUILD_SHARED_LIBS=ON 
+${KokkosKernels_CUDA_ARGS} \
+-DBUILD_SHARED_LIBS=ON
 
 cmake --build build -j $CPU_COUNT
 
