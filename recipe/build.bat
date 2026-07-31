19eba7d4803f5a6792c3a25550a6ae6375af1dc0
Felipe Bordeu <felipebordeu@gmail.com>
2026-07-31 10:43:53 +0930

    work on windows compilation test

--- recipe/build.bat
+++ recipe/build.bat
@@ -7,7 +7,9 @@ cmake %CMAKE_ARGS% -S %SRC_DIR% -B build ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
   -DCMAKE_INSTALL_LIBDIR=lib ^
-  -DBUILD_SHARED_LIBS=ON
+  -DBUILD_SHARED_LIBS=ON ^
+  -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
+  -DKokkosKernels_ADD_DEFAULT_ETI=OFF
 if errorlevel 1 exit /b 1
 
 cmake --build build --parallel %CPU_COUNT%
